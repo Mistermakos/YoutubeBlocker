280f272085c73ca7df6fbeb22ff243bb12d017e2
Maciej Grzesiak <107268659+Mistermakos@users.noreply.github.com>
2026-03-18 14:13:54 +0100

    1st level done!

--- YoutubeBlocker.js
+++ YoutubeBlocker.js
@@ -7,46 +7,55 @@
 // @match        https://www.youtube.com/*
 // @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
 // @grant        none
-// @run-at document-idle
 // ==/UserScript==
 
-const HomePageVideosBlocker = () => {
-    const targetNode = document.body;
-    const config = { childList: true, subtree: true };
-
-    const callback = (mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if (mutation.type === "childList") {
-                document.getElementsByTagName("ytd-rich-grid-renderer")[0].style.display = "none";
-                console.log("Works!");
-            };
+const noShorts = () => {
+    //Waits for changes (dynamicly loaded page requires this), then disconects.
+    let observer = new MutationObserver((mutations, obs) => {
+        let el = document.querySelector("[is-shorts]");
+        if (el) {
+            el.style.display = "none"
+            obs.disconnect()
         }
-    };
-
-    const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-}
-
-const ShortsBlocker = () => {
-    const targetNode = document.body;
-    const config = { childList: true, subtree: true };
-
-    const callback = (mutationList, observer) => {
-        for (const mutation of mutationList) {
-            if (mutation.type === "childList") {
-                document.querySlector("[is-shorts]").style.display = "none";
-                console.log("Works on shorts!");
-            };
-        }
-    };
-
-    const observer = new MutationObserver(callback);
-    observer.observe(targetNode, config);
-}
-
-(function() {
-    window.addEventListener('load', () => {
-        HomePageVideosBlocker();
-        ShortsBlocker();
     });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    })
+}
+
+const noVideosOnMainSite = () => {
+    //Waits for changes (dynamicly loaded page requires this), then disconects.
+    let observer = new MutationObserver((mutations, obs) => {
+        let el = document.body;
+        if (el) {
+            document.getElementsByTagName("ytd-rich-grid-renderer")[0].style.display = "none";
+            obs.disconnect()
+        }
+    });
+
+    observer.observe(document.body, {
+        childList: true,
+        subtree: true
+    });
+}
+
+(function () {
+    //dict: path -> function
+    const routes = {
+        "/": () => {
+            noVideosOnMainSite();
+        },
+        "/feed/subscriptions": () => {
+            noShorts();
+        }
+    }
+
+    const runRoute = () => {
+        routes[window.location.pathname]();
+    }
+
+    window.addEventListener('yt-navigate-finish', runRoute);
+    runRoute();
 })();
